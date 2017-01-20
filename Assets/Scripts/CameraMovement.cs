b023f156445de650a9379479eb9ed56e5718752e
MalloryEaton <meaton@harding.edu>
2017-01-19 22:47:54 -0600

    File Structure and Camera Script
    Changed the file structure to be easier to understand. Also made a small
    temporary script for moving the camera around a scene.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public float speed = 5.0f;
+    public float rotationSpeed = 2.0f;
+    public float inOutSpeed = 10.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -inOutSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, inOutSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+
+        yaw += rotationSpeed * Input.GetAxis("Mouse X");
+        pitch -= rotationSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
