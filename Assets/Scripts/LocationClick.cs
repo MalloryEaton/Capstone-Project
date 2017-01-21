9fdd619a9a1411561a53638db4349991ff9acee4
MalloryEaton <meaton@harding.edu>
2017-01-21 02:46:14 -0600

    Added asset for orbs and worked on making the game board
    The camera script has been updated to toggle on and off by pressing the
    key 'C'. The playable locations can be clicked and a message with the
    location's number will show in the console.

---
+++ Assets/Scripts/LocationClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationClick : MonoBehaviour {
+    public short locationNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    //detect click
+    private void OnMouseDown()
+    {
+        Debug.Log("Location Number: " + locationNumber);
+    }
+}
