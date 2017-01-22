cbe9a6a4892d4e7340163e1729b909f0c914472f
Braden Madgett <bmadgett@harding.edu>
2017-01-21 19:58:38 -0600

    Logos

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.ImageEffects;
+
+public class ButtonController : MonoBehaviour {
+
+    public GameObject Menu;
+
+	public void StartGame (string scene)
+    {
+        Menu.SetActive(false);
+    }
+}
