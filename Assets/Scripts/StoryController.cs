63b35b089ec5839d5225ec0e4e29887e0a8b6e57
Braden Madgett <bmadgett@harding.edu>
2017-01-23 21:36:22 -0600

    MainMenu + Animation
    Added MainMenu Scene with Slide In Animation

---
+++ Assets/Scripts/StoryController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryController : MonoBehaviour {
+
+    public GameObject StoryPanel;
+
+    void Awake()
+    {
+        StoryPanel.GetComponent<Animator>().SetBool("isDisplayed", false);
+        print("awake");
+    }
+
+    public void display()
+    {
+        StoryPanel.GetComponent<Animator>().SetBool("isDisplayed", true);
+    }
+
+    public void hide()
+    {
+        StoryPanel.GetComponent<Animator>().SetBool("isDisplayed", false);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
