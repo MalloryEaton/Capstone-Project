fcd4a10e08317c28b858395e4fd3820a86f48eab
Braden Madgett <bmadgett@harding.edu>
2017-01-28 14:36:09 -0600

    CharacterSelectSceneTesting

---
+++ Assets/ClickTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTest : MonoBehaviour {
+
+    public GameObject BlueMage;
+
+    void OnMouseDown()
+    {
+        print("clicked");
+    }
+}
