d2f48316383c4b1349164fc6916a5fbd166d9e0d
Braden Madgett <bmadgett@harding.edu>
2017-04-08 17:25:06 -0500

    Lobby Waiting for opponent prompt

--- Assets/Scripts/UIControllers/LobbyController.cs
+++ Assets/Scripts/UIControllers/LobbyController.cs
@@ -8,17 +8,31 @@ public class LobbyController : MonoBehaviour {
 
     public GameObject LoadingScreen;
     public Button joinGameButton;
+    public GameObject waitingForOpponent;
 
     private void Start()
     {
         print("Load Lobby");
         PlayerPrefs.SetString("GameType", "Network");
         joinGameButton.interactable = false;
+        waitingForOpponent.SetActive(false);
     }
 
     public void returnToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void displayWaitingForOpponent()
+    {
+        waitingForOpponent.SetActive(true);
+        waitingForOpponent.GetComponent<Animator>().SetBool("isDisplayed", true);
+    }
+
+    public void hideWaitingForOpponent()
+    {
+        waitingForOpponent.GetComponent<Animator>().SetBool("isDisplayed", false);
+        waitingForOpponent.SetActive(false);
     }
 
 }
